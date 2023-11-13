98ca4951d3d0bc4b654204562edc2eca87f27dfb
utaysi <utaysi@gmail.com>
2023-11-13 20:11:43 +0100

    cleanup

--- main.R
+++ main.R
@@ -5,31 +5,34 @@ File = ReadTS(FileName = "Energy_blend_rawdata2784724") #Make sure to change the
 # Skip initial lines, set separator, manually set column names, read/store the data
 #data <- read.table("Energy_blend_rawdata2784724ts.ts",  sep = "\t", skip = 4, header = FALSE, col.names = c("UnixTime", "Price", "TimeInserted", "EntryID", "Volume"))
 
-#splitting the columns
-File$Series[1:7,]
-File$FurtherTexts[1:7,]
-Prices = File$Series[,2]
+#Price, UnixTime and UnixTimeSorted Separated
+Price = File$Series[,2]
 UnixTime =File$Series[,1]
-UnixTime1= order(File$Series[,1])
+UnixTimeSorted= order(File$Series[,1])
 
-#View Structure
-str(File)
 #removing duplicated indexes
-Uniqueindexes= which(!duplicated(x = UnixTime1))
+UniqueIndexes= which(!duplicated(x = UnixTimeSorted))
+
+plot_ly(x = UnixTime[UnixTimeSorted[UniqueIndexes]], y= Price[UnixTimeSorted[UniqueIndexes]], type = "scatter", mode = "lines" )
+
+
+
+
+
 ##########################################################
 
 # Filter NaN values
-data_cleaned <- File %>%filter(!is.na(UnixTime) & !is.na(Price))
+#data_cleaned <- File %>%filter(!is.na(UnixTime) & !is.na(Price))
 
 # Create a copy of the original data
-data_TimeConverted = data_cleaned
+#data_TimeConverted = data_cleaned
 #test
-data_TimeConverted2 = data_cleaned
+#data_TimeConverted2 = data_cleaned
 
 # Convert UnixTime to POSIXct (R Date-Time class) with Chicago timezone
-data_TimeConverted$UnixTime <- as.POSIXct(data_TimeConverted$UnixTime, origin = "1970-01-01", tz = "America/Chicago")
+#data_TimeConverted$UnixTime <- as.POSIXct(data_TimeConverted$UnixTime, origin = "1970-01-01", tz = "America/Chicago")
 #test
-data_TimeConverted2$UnixTime <- as.POSIXct(data_TimeConverted2$UnixTime, origin = "1970-01-01", tz = "America/Chicago")
+#data_TimeConverted2$UnixTime <- as.POSIXct(data_TimeConverted2$UnixTime, origin = "1970-01-01", tz = "America/Chicago")
 
 # Convert the time zone to UTC
-data_TimeConverted$UnixTime <- with_tz(data_TimeConverted$UnixTime, tzone = "UTC")
+#data_TimeConverted$UnixTime <- with_tz(data_TimeConverted$UnixTime, tzone = "UTC")
--- setup.R
+++ setup.R
@@ -2,8 +2,10 @@ install.packages("dplyr")
 install.packages("lubridate")
 install.packages("remotes")
 remotes::install_github("Mthrun/TSAT")
+install.packages("plotly")
 
 
 library(dplyr)
 library(lubridate)
 library(TSAT)
+library(plotly)
