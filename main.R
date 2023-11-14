05ecfe34baa257d7b9263cfa25d4f38466accb03
KvindtEva <46592235+KvindtEva@users.noreply.github.com>
2023-11-14 15:48:47 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,4 +1,5 @@
 
+# Install the dplyr package if not installed
 if (!requireNamespace("dplyr", quietly = TRUE)) {
   install.packages("dplyr")
 }
@@ -12,53 +13,88 @@ if (!requireNamespace("TSAT", quietly = TRUE)) {
 library(TSAT)
 
 # Read the time series data from file using the ReadTS function
-File <- ReadTS(FileName = "Energy_blend_rawdata2784724ts", InDirectory = "ur file path ")
- #Make sure to change the original filename to "Energy_blend_rawdata2784724.ts"
+# Make sure to change the original filename to "Energy_blend_rawdata2784724.ts"
+# Change the name of your directory
+ur_file_dir <- "C:/Users/evaqw/Pictures/UniMarburg/DM/EnergyTimeSeriesAnalysis-master/EnergyTimeSeriesAnalysis-master"
+File <- ReadTS(FileName = "Energy_blend_rawdata2784724", InDirectory = ur_file_dir)
 
 # Combine 'Series' and 'FurtherTexts' from File into a single dataframe
 combined_data <- as.data.frame(cbind(File$Series, File$FurtherTexts))
 
 # Process filtered_data by arranging and summarizing
 filtered_data <- combined_data %>%
+  
   # Sort the data by UnixTime in ascending order. If there are duplicates,
   # sort these by EntryID in descending order, then by TimeInserted in descending order.
   arrange(UnixTime, desc(EntryID), desc(TimeInserted)) %>%
+  
   # Group the data by UnixTime to process each time point separately.
   group_by(UnixTime) %>%
+  
   # Create a summary for each group (each unique UnixTime).
   summarise(
-    # Determine the Price for each UnixTime.
+  
+      # Determine the Price for each UnixTime.
+    
     # If there's only one entry or all entries have the same price, use the first price.
     Price = ifelse(n() == 1 || length(unique(Price)) == 1,
+                   
                    first(Price),
+                   
                    # If all EntryID values are NA or the same, then check TimeInserted.
                    ifelse(all(is.na(EntryID)) || length(unique(EntryID)) == 1,
+                          
                           # If all TimeInserted values are NA or the same, then check price differences.
                           ifelse(all(is.na(TimeInserted)) || length(unique(TimeInserted)) == 1,
+                                 
                                  # If the price difference is less than 0.05, calculate the mean; otherwise, set as NA.
-                                 ifelse(abs(diff(Price)) < 0.05, mean(Price, na.rm = TRUE), NA),
+                                 ifelse(abs(diff(Price)) < 0.05, 
+                                        mean(Price, na.rm = TRUE), 
+                                        NA
+                                        ),
+                                 
                                  # Select the first non-NA price.
-                                 first(Price[!is.na(Price)])),
+                                 first(Price[!is.na(Price)])
+                                 ),
+                          
                           # Select the first non-NA price if the EntryID check doesn't lead to a conclusion.
-                          first(Price[!is.na(Price)]))),
+                          first(Price[!is.na(Price)])
+                          )
+                   ),
+    
     # Select the first non-NA EntryID for each UnixTime. If all are NA, set as NA.
-    EntryID = ifelse(all(is.na(EntryID)), NA, first(EntryID[!is.na(EntryID)])),
+    EntryID = ifelse(all(is.na(EntryID)), 
+                     NA, 
+                     first(EntryID[!is.na(EntryID)])
+                     ),
+    
     # Select the first non-NA TimeInserted for each UnixTime. If all are NA, set as NA.
-    TimeInserted = ifelse(all(is.na(TimeInserted)), NA, first(TimeInserted[!is.na(TimeInserted)])),
+    TimeInserted = ifelse(all(is.na(TimeInserted)), 
+                          NA, 
+                          first(TimeInserted[!is.na(TimeInserted)])
+                          ),
+    
     # Select the first non-NA Volume for each UnixTime. If all are NA, set as NA.
-    Volume = ifelse(all(is.na(Volume)), NA, first(Volume[!is.na(Volume)]))
+    Volume = ifelse(all(is.na(Volume)), 
+                    NA, 
+                    first(Volume[!is.na(Volume)])
+                    )
+  
   ) %>%
+  
   # Remove the grouping structure from the data.
   ungroup()
 
 
 # Convert UnixTime from seconds since epoch to actual date-time in UTC, then convert to Date format.
-filtered_data <- filtered_data %>%
+filtered_data <- filtered_data %>% 
   mutate(Date = as.Date(as.POSIXct(UnixTime, origin = "1970-01-01", tz = "UTC")))
 
 # Group the data by the new Date column to perform operations on each day's data.
 daily_data <- filtered_data %>%
+  
   group_by(Date) %>%
+  
   summarise(
     # Get the opening price and volume (first entries of the day)
     OpeningPrice = first(Price),
@@ -67,27 +103,47 @@ daily_data <- filtered_data %>%
     ClosingPrice = last(Price),
     ClosingVolume = last(Volume)
   ) %>%
+  
   # Remove the group by structure from the data to return it to a regular dataframe.
   ungroup()
 
+# Change name of your output directory
+out_directory <- "C:/Users/evaqw/Pictures/UniMarburg/DM/EnergyTimeSeriesAnalysis-master/EnergyTimeSeriesAnalysis-master"
 
-# Save the univariate time series in daily resolution using WriteDates
-WriteDates(
-  FileName = "the_univariate_time_series",
+# Save the univariate time series (all dataset) in daily resolution using WriteDates 
+# as .csv with first column in Date format
+TSAT::WriteDates(
+  FileName = "preprocessed_data",
   TSdata = daily_data,
-  Key = 1:ncol(daily_data),
-  OutDirectory = "C:/Users/Dell1/Downloads"
+  Key = 1:nrow(daily_data),
+  OutDirectory = out_directory
 )
 
-# Insert NaN for days with no opening price
-daily_data$OpeningPrice[is.na(daily_data$OpeningPrice)] <- NaN
-
-daily_data$TimeInserted <- as.numeric(as.POSIXct(daily_data$Date, origin = "1970-01-01", tz = "UTC"))
+daily_data$Date <- as.numeric(difftime(daily_data$Date, as.Date("1970-01-01"), units = "secs"))
 
 # Save the preprocessed data in the same format using the TSAT::WriteTS function
-TSAT::WriteTS(FileName = "preprocessed_data",SeriesVal = as.character(daily_data$OpeningPrice),Time = daily_data$TimeInserted, OutDirectory = "C:/Users/Dell1/Downloads" #change it to your path)
-              ,
-              # Read the preprocessed data
-              preprocessed_data <- ReadTS(FileName = "preprocessed_data", InDirectory = "C:/Users/Dell1/Downloads") #change it to your path
-)
+# as a .ts file with the first column of dates in Unix format
+TSAT::WriteTS(FileName = "ts_opening_price",
+              SeriesVal = as.character(daily_data$OpeningPrice),
+              Time = daily_data$Date,
+              OutDirectory = out_directory)
+
+TSAT::WriteTS(FileName = "ts_opening_volume",
+              SeriesVal = as.character(daily_data$OpeningVolume),
+              Time = daily_data$Date,
+              OutDirectory = out_directory)
+
+TSAT::WriteTS(FileName = "ts_closing_price",
+              SeriesVal = as.character(daily_data$ClosingPrice),
+              Time = daily_data$Date,
+              OutDirectory = out_directory)
+
+TSAT::WriteTS(FileName = "ts_closing_volume",
+              SeriesVal = as.character(daily_data$ClosingVolume),
+              Time = daily_data$Date,
+              OutDirectory = out_directory)
+
+# Read the preprocessed data (Just for testing)
+preprocessed_data <- ReadTS(FileName = "ts_opening_price", 
+                            InDirectory = out_directory)
 
